15760ef324fed111a706ffddfd8efe95d92aedc9
Angel De La Cruz <angeldelacruzdev@gmail.com>
2024-05-14 23:37:06 -0400

    Fin de actualizacion de server component

--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -1,16 +1,29 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { TodosProps } from "../interfaces/todos-props.interface";
 import { TodoItem } from "./TodoItem";
 
 import { updateTodo } from "@/todos/helpers/todo";
 
 export const TodosGrid = ({ todos = [] }: TodosProps) => {
+
+  const router = useRouter();
+
+
+  const toggleTodo = async (id: string, complete: boolean) => {
+    console.log(id, complete)
+    const updateTodoItem = await updateTodo(id, complete)
+    console.log(updateTodoItem)
+    router.refresh()
+
+  }
+
   return (
     <>
       <div className="grid grid-cols-12 gap-2">
         {todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} toggleTodo={updateTodo} />
+          <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
         ))}
       </div>
     </>
